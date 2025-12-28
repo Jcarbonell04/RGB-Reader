b781590697c292685c20bd9e850940763c85b670
Jaedyn <61067221+Jcarbonell04@users.noreply.github.com>
2025-12-27 18:41:01 -0700

    Formatting

--- software/README.md
+++
@@ -1,3 +0,0 @@
-# Software
-
-All files related to software development should appear in this folder. There may be separate versions for the prototyping phase and PCB phase.
--- testing_TCS34725/Output/Debug/Obj/g031_test/stm32g031xx_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/g031_test/stm32g031xx_Vectors.o: \
- C:\Users\jcarb\Documents\GitHub\CMPE2750\Instructors\ Content\Libraries_Simon-main\test_g031_i2c_oled_hello\STM32G0xx\Source\stm32g031xx_Vectors.s
